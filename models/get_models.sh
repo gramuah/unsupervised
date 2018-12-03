83b51f596ef4f213b258a37a031ee226982edc8a
Rober <robertoj.lopez@uah.es>
2018-12-03 10:39:25 +0100

    updated file size

--- models/get_models.sh
+++ models/get_models.sh
@@ -3,7 +3,7 @@
 #Script to fetch all the pretrained models
 
 #Models for UCF101
-echo "Downloading pretrained models (4.2 GB)"
+echo "Downloading pretrained models (3.1 GB)"
 
 FILE=models.tar.gz
 URL=http://agamenon.tsc.uah.es/Personales/rlopez/data/unsupervised/models.tar.gz
